82cdc0686d89006b0754d813e243d8a81ab977b5
JohnLenon22 <joonleno122213@gmail.com>
2025-11-26 15:06:31 -0300

    comeco de test de integracao  movimentacao estoque

--- backend_clean/tests/integrations/MovimentacaoEstoque.test.ts
+++ backend_clean/tests/integrations/MovimentacaoEstoque.test.ts
@@ -2,15 +2,27 @@ import request from "supertest"
 import { app } from "../../src/app"
 import { prisma } from "../../src/infraestructure/prisma/client"
 
-describe("Teste de Integração - Produtos", () => {
+describe("Teste de Integração - Movimentação Estoque", () => {
     beforeEach(async () => {
-    await prisma.produto.deleteMany();
+        await prisma.movimentacaoEstoque.deleteMany();
     });
 
     afterAll(async () => {
         await prisma.$disconnect();
     });
 
-    it("deve criar um movimentacao corretamente")
+    it("deve criar um movimentacao estoque corretamente", async () => {
+        const input = {
+            idProduto: "cd278e51-ea84-4007-a846-35ba2475e904",
+            idUsuario: "a5f55eb0-a37e-4e28-8d9f-f32b53d13985",
+            idLocalArmazenamento: "dbbf80c9-5895-4fdf-861f-7275c2b2e6a4",
+            tipoMovimentacao: "ENTRADA",
+            quantidade: 900
+
+        }
+        const res = await request(app).post("/movimentInventories/").send(input)
+
+        expect(res.status).toBe(201)
+    })
 
 })
--- backend_clean/tests/integrations/produto.test.ts
+++ backend_clean/tests/integrations/produto.test.ts
@@ -42,93 +42,93 @@ describe("Teste de Integração - Produtos", () => {
         expect(res.body.error).toBe("Preço de compra e preço de venda devem ser maiores que zero")
     });
 
-        it('deve retornar erro caso preço de venda for menor que preço de compra', async () => {
-            const input = {
-                nome: "Sim2",
-                quantidade: 10,
-                precoVenda: 1.0,
-                precoCompra: 2.0,
-                descricao: "sim",
-                idCategoria: 1
-            };
+    it('deve retornar erro caso preço de venda for menor que preço de compra', async () => {
+        const input = {
+            nome: "Sim2",
+            quantidade: 10,
+            precoVenda: 1.0,
+            precoCompra: 2.0,
+            descricao: "sim",
+            idCategoria: 1
+        };
 
-            const res = await request(app).post("/products").send(input)
+        const res = await request(app).post("/products").send(input)
 
-            expect(res.status).toBe(400)
-            expect(res.body.error).toBe("O preço de venda do produto deve ser maior que o preço de compra")
+        expect(res.status).toBe(400)
+        expect(res.body.error).toBe("O preço de venda do produto deve ser maior que o preço de compra")
 
-        });
-    
-    
-        it('deve retornar erro caso a quantidade seja menor que 0', async () => {
-            const input = {
-                nome: "Sim2",
-                quantidade: -1,
-                precoVenda: 3.0,
-                precoCompra: 2.0,
-                descricao: "sim",
-                idCategoria: 1
-            };
+    });
 
-            const res = await request(app).post("/products").send(input)
 
-            expect(res.status).toBe(400)
-            expect(res.body.error).toBe("A quantidade do produto deve ser maior ou igual a zero")
+    it('deve retornar erro caso a quantidade seja menor que 0', async () => {
+        const input = {
+            nome: "Sim2",
+            quantidade: -1,
+            precoVenda: 3.0,
+            precoCompra: 2.0,
+            descricao: "sim",
+            idCategoria: 1
+        };
 
-        });
-    
-    
-        it('deve retornar erro caso nome for vazio', async () => {
-            const input = {
-                nome: "   ",
-                quantidade: 10,
-                precoVenda: 3.0,
-                precoCompra: 2.0,
-                descricao: "sim",
-                idCategoria: 1
-            };
+        const res = await request(app).post("/products").send(input)
 
-            const res = await request(app).post("/products").send(input)
+        expect(res.status).toBe(400)
+        expect(res.body.error).toBe("A quantidade do produto deve ser maior ou igual a zero")
 
-            expect(res.status).toBe(400)
-            expect(res.body.error).toBe("O nome do produto não pode ser vazio")
-        });
-    
-    
-        it('deve retornar erro caso data seja futura', async () => {
-            const input = {
-                nome: "asd",
-                dataCadastro: "4025-11-11",
-                quantidade: 10,
-                precoVenda: 3.0,
-                precoCompra: 2.0,
-                descricao: "sim",
-                idCategoria: 1
-            };
+    });
 
-            const res = await request(app).post("/products").send(input)
 
-            expect(res.status).toBe(400)
-            expect(res.body.error).toBe("A data de cadastro não pode ser uma data futura")
-        });
+    it('deve retornar erro caso nome for vazio', async () => {
+        const input = {
+            nome: "   ",
+            quantidade: 10,
+            precoVenda: 3.0,
+            precoCompra: 2.0,
+            descricao: "sim",
+            idCategoria: 1
+        };
 
-        it('deve retornar erro caso data seja inválida', async () => {
-            const input = {
-                nome: "asd",
-                dataCadastro: "20$5-11-11",
-                quantidade: 10,
-                precoVenda: 3.0,
-                precoCompra: 2.0,
-                descricao: "sim",
-                idCategoria: 1
-            };
+        const res = await request(app).post("/products").send(input)
 
-            const res = await request(app).post("/products").send(input)
+        expect(res.status).toBe(400)
+        expect(res.body.error).toBe("O nome do produto não pode ser vazio")
+    });
 
-            expect(res.status).toBe(400)
-            expect(res.body.error).toBe("A data de cadastro é inválida")
-        });
-    
+
+    it('deve retornar erro caso data seja futura', async () => {
+        const input = {
+            nome: "asd",
+            dataCadastro: "4025-11-11",
+            quantidade: 10,
+            precoVenda: 3.0,
+            precoCompra: 2.0,
+            descricao: "sim",
+            idCategoria: 1
+        };
+
+        const res = await request(app).post("/products").send(input)
+
+        expect(res.status).toBe(400)
+        expect(res.body.error).toBe("A data de cadastro não pode ser uma data futura")
+    });
+
+    it('deve retornar erro caso data seja inválida', async () => {
+        const input = {
+            nome: "asd",
+            dataCadastro: "20$5-11-11",
+            quantidade: 10,
+            precoVenda: 3.0,
+            precoCompra: 2.0,
+            descricao: "sim",
+            idCategoria: 1
+        };
+
+        const res = await request(app).post("/products").send(input)
+
+        expect(res.status).toBe(400)
+        expect(res.body.error).toBe("A data de cadastro é inválida")
+    });
+
     
 
 
@@ -138,7 +138,6 @@ describe("Teste de Integração - Produtos", () => {
 
         expect(res.status).toBe(200)
     })
-
 
 })
 
